26544f4e2edddd37abf0462fb33ce827ed70bdcd
Salah El Hazzat <salaheddine.elhazzat@gmail.com>
2020-08-31 09:44:46 +0200

    Undo space changes in original code

--- mlmodels/util_json.py
+++ mlmodels/util_json.py
@@ -3,16 +3,14 @@
 Alll related to json dynamic parsing
 
 
-"""  # -*- coding: utf-8 -*-
+"""# -*- coding: utf-8 -*-
 import os
 import re
 import fnmatch
 
 # import toml
 from pathlib import Path
-from jsoncomment import JsonComment;
-
-json = JsonComment()
+from jsoncomment import JsonComment ; json = JsonComment()
 
 import importlib
 from inspect import getmembers
@@ -58,15 +56,14 @@ def os_package_root_path(filepath="", sublevel=0, path_add=""):
 
 ###################################################################################################
 def params_json_load(path, config_mode="test",
-                     tlist=["model_pars", "data_pars", "compute_pars", "out_pars"]):
-    from jsoncomment import JsonComment;
-    json = JsonComment()
+                     tlist= [ "model_pars", "data_pars", "compute_pars", "out_pars"] ):
+    from jsoncomment import JsonComment ; json = JsonComment()
     pars = json.load(open(path, mode="r"))
     pars = pars[config_mode]
 
     ### HyperParam, model_pars, data_pars,
     list_pars = []
-    for t in tlist:
+    for t in tlist :
         pdict = pars.get(t)
         if pdict:
             list_pars.append(pdict)
@@ -75,12 +72,12 @@ def params_json_load(path, config_mode="test",
 
     return tuple(list_pars)
 
-
 #########################################################################################
 #########################################################################################
 def load_function(package="mlmodels.util", name="path_norm"):
-    import importlib
-    return getattr(importlib.import_module(package), name)
+  import importlib
+  return  getattr(importlib.import_module(package), name)
+
 
 
 def load_function_uri(uri_name="path_norm"):
@@ -105,31 +102,31 @@ def load_function_uri(uri_name="path_norm"):
 
     try:
         #### Import from package mlmodels sub-folder
-        return getattr(importlib.import_module(package), name)
+        return  getattr(importlib.import_module(package), name)
 
     except Exception as e1:
         try:
             ### Add Folder to Path and Load absoluate path module
             path_parent = str(Path(package).parent.parent.absolute())
             sys.path.append(path_parent)
-            # log(path_parent)
+            #log(path_parent)
 
             #### import Absolute Path model_tf.1_lstm
-            model_name = Path(package).stem  # remove .py
+            model_name   = Path(package).stem  # remove .py
             package_name = str(Path(package).parts[-2]) + "." + str(model_name)
-            # log(package_name, model_name)
-            return getattr(importlib.import_module(package_name), name)
+            #log(package_name, model_name)
+            return  getattr(importlib.import_module(package_name), name)
 
         except Exception as e2:
             raise NameError(f"Module {pkg} notfound, {e1}, {e2}")
 
 
 def load_callable_from_uri(uri):
-    assert (len(uri) > 0 and ('::' in uri or '.' in uri))
+    assert(len(uri)>0 and ('::' in uri or '.' in uri))
     if '::' in uri:
         module_path, callable_name = uri.split('::')
     else:
-        module_path, callable_name = uri.rsplit('.', 1)
+        module_path, callable_name = uri.rsplit('.',1)
     if os.path.isfile(module_path):
         module_name = '.'.join(module_path.split('.')[:-1])
         spec = importlib.util.spec_from_file_location(module_name, module_path)
@@ -145,7 +142,7 @@ def load_callable_from_dict(function_dict, return_other_keys=False):
     uri = function_dict.pop('uri')
     func = load_callable_from_uri(uri)
     try:
-        assert (callable(func))
+        assert(callable(func))
     except:
         raise TypeError(f'{func} is not callable')
     arg = function_dict.pop('arg', {})
@@ -155,37 +152,35 @@ def load_callable_from_dict(function_dict, return_other_keys=False):
         return func, arg, function_dict
 
 
+
+
 def test_functions_json(arg=None):
-    from mlmodels.util import load_function_uri
+  from mlmodels.util import load_function_uri
 
-    path = path_norm("dataset/test_json/test_functions.json")
-    dd = json.load(open(path))['test']
+  path = path_norm("dataset/test_json/test_functions.json")
+  dd   = json.load(open( path ))['test']
 
-    for p in dd:
-        try:
-            log("\n\n", "#" * 20, p)
+  for p in dd  :
+     try :
+         log("\n\n","#"*20, p)
 
-            myfun = load_function_uri(p['uri'])
-            log(myfun)
+         myfun = load_function_uri( p['uri'])
+         log(myfun)
 
-            w = p.get('args', [])
-            kw = p.get('kw_args', {})
+         w  = p.get('args', [])
+         kw = p.get('kw_args', {} )
 
-            if len(kw) == 0 and len(w) == 0:
-                log(myfun())
+         if len(kw) == 0 and len(w) == 0   : log( myfun())
 
-            elif len(kw) > 0 and len(w) > 0:
-                log(myfun(*w, **kw))
+         elif  len(kw) > 0 and len(w) > 0  : log( myfun( *w,  ** kw ))
 
-            elif len(kw) > 0 and len(w) == 0:
-                log(myfun(**kw))
+         elif  len(kw) > 0 and len(w) == 0 : log( myfun( ** kw ))
 
-            elif len(kw) == 0 and len(w) > 0:
-                log(myfun(*w))
+         elif  len(kw) == 0 and len(w) > 0 : log( myfun( *w ))
 
 
-        except Exception as e:
-            log(e, p)
+     except Exception as e:
+        log(e, p )
 
 
 import json
